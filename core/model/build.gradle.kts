a00b8bcdbd2fe212c1f994f0f6eaa6e3e028af90
Muhammad Abdi Alidrus <113153953+abdimuhammad@users.noreply.github.com>
2023-06-17 21:13:31 +0700

    update Java version

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
 }
-
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
