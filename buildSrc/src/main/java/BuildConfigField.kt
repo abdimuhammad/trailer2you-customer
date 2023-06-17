ffa2b4ed25419719c75f14dfb0e12481b7c2830e
Muhammad Abdi Alidrus <113153953+abdimuhammad@users.noreply.github.com>
2023-06-17 21:09:57 +0700

    create BuildConfigField

---
+++ buildSrc/src/main/java/BuildConfigField.kt
@@ -0,0 +1,8 @@
+object BuildConfigField {
+    const val DEV_BASE_URL = "\"https://api.dev.admin.trailer2you.com.au/v1/\""
+    const val DEV_STRIPE_API_KEY = "\"pk_test_51HCsxqGLBR8nomTlmnpEx2ZJnVduO1hAR6hZtDdNQxlkyeaRBaobtSE8ypCRXLAScUswLzCm2l0TF2qlOgn2CDm800dgEJDHRM\""
+    const val DEV_WEB_SOCKET_BASE_URL = "\"https://api.dev.admin.trailer2you.com.au\""
+    const val PROD_BASE_URL = "\"https://api.admin.trailer2you.com.au/v1/\""
+    const val PROD_STRIPE_API_KEY = "\"pk_live_51HCsxqGLBR8nomTlYN1alWdZbxKpiCOd5lANfqDMfDMONyL8sqOsRqdmfE6szt2adi2PCYnu8f6TJco1N8eLnRmD00kMo7Fusa\""
+    const val PROD_WEB_SOCKET_BASE_URL = "\"https://api.admin.trailer2you.com.au\""
+}
