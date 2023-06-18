f25310222c37e038b8ea9b309f57f423451f0d95
Muhammad Abdi Alidrus <113153953+abdimuhammad@users.noreply.github.com>
2023-06-18 09:03:13 +0700

    create SharedPreferences extension class

---
+++ core/common/src/main/java/apps/trailer2you/common/extension/SharedPreferencesExt.kt
@@ -0,0 +1,22 @@
+package apps.trailer2you.common.extension
+
+import android.content.SharedPreferences
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+fun SharedPreferences.string(
+    defaultValue: String? = null,
+    key: (KProperty<*>) -> String = KProperty<*>::name
+): ReadWriteProperty<Any, String?> =
+    object : ReadWriteProperty<Any, String?> {
+        override fun getValue(
+            thisRef: Any,
+            property: KProperty<*>
+        ) = getString(key(property), defaultValue)
+
+        override fun setValue(
+            thisRef: Any,
+            property: KProperty<*>,
+            value: String?
+        ) = edit().putString(key(property), value).apply()
+    }
